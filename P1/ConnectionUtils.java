03c9bfe5d3d9a8b99e6038dc1d7922de1401db04
M K <mkpro118@gmail.com>
2024-02-05 21:16:17 -0600

    Connections utils implemented

--- P1/ConnectionUtils.java
+++ P1/ConnectionUtils.java
@@ -1,21 +1,59 @@
 import java.net.Socket;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class ConnectionUtils {
-    public static Socket createSocket(String hostname, int port) {
-        // Implement socket creation logic
+    private final static byte[] dataBuffer;
+
+    static {
+        dataBuffer = new byte[Constants.CHUNK_SIZE.getValue()];
+    }
+
+    public final static Socket createSocket(String hostname, int port) {
+        try {
+            return new Socket(hostname, port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error creating socket: " + e.getMessage());
+            System.exit(1);
+        }
+
         return null;
     }
 
-    public static void closeSocket(Socket socket) {
-        // Implement socket closing logic
+    public final static void closeSocket(Socket socket) {
+        try {
+            if (!socket.isClosed())
+                socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error closing socket: " + e.getMessage());
+            System.exit(1);
+        }
     }
 
-    public static void sendData(Socket socket, byte[] data) {
-        // Implement data sending logic
+    public final static void sendData(Socket socket) {
+        try (OutputStream outputStream = socket.getOutputStream()) {
+            outputStream.write(dataBuffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error sending data: " + e.getMessage());
+            System.exit(1);
+        }
     }
 
-    public static byte[] receiveData(Socket socket) {
-        // Implement data receiving logic
+    public final static byte[] receiveData(Socket socket) {
+        try (InputStream inputStream = socket.getInputStream()) {
+            inputStream.read(dataBuffer);
+
+            return dataBuffer;
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error receiving data: " + e.getMessage());
+            System.exit(1);
+        }
+
         return null;
     }
 }
