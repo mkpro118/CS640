2d39c5d691243caed42452bfd0907fbb9e23fdcc
M K <mkpro118@gmail.com>
2024-02-07 09:51:56 -0600

    Added docs for `ServerConfig`

--- P1/ServerConfig.java
+++ P1/ServerConfig.java
@@ -1,1 +1,9 @@
+/**
+ * Represents configuration parameters for an Iperfer server
+ *
+ * This record represents a configuration for an Iperfer server, including
+ * the port number to listen on.
+ *
+ * @param listenPort the port number of the server
+ */
 public record ServerConfig(int listenPort) {}
