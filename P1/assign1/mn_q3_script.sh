f45d5118b0b51d10d5cca5c042edf72d26055585
M K <mkpro118@gmail.com>
2024-02-12 09:11:29 -0600

    Fixed Syntax error

--- P1/assign1/mn_q3_script.sh
+++ P1/assign1/mn_q3_script.sh
@@ -13,8 +13,8 @@ h4 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.1 -p 5001 -
 h9 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.7 -p 5007 -t 20 > MP_h9_h7_1 &
 
 c0 echo "Waiting for tests to complete"
-h4 wait ($jobs -p)
-h9 wait ($jobs -p)
+h4 wait
+h9 wait
 
 c0 echo "Running Tests: h4 -> h1  +  h9 -> h7 + h10 -> h8"
 h4 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.1 -p 5001 -t 20 > MP_h4_h1_2 &
@@ -22,8 +22,8 @@ h9 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.7 -p 5007 -
 h10 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.8 -p 5008 -t 20 > MP_h10_h8_2 &
 
 c0 echo "Waiting for tests to complete"
-h4 wait ($jobs -p)
-h9 wait ($jobs -p)
-h10 wait ($jobs -p)
+h4 wait
+h9 wait
+h10 wait
 
 c0 echo "Multiplexing Tests Complete!"
