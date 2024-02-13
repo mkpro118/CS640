507a79cfa9b0197e4e1df7d62649f89cf08c9c02
M K <mkpro118@gmail.com>
2024-02-13 12:09:40 -0600

    dots for q2, fancy

--- P1/assign1/mn_q2_script.sh
+++ P1/assign1/mn_q2_script.sh
@@ -28,24 +28,24 @@ h3 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -s -p 5002 &
 h3 echo 'Server on h3 is up!'
 c0 echo -e '\nAll servers up and running\n'
 
-h1 echo -n 'Running Iperfer client on link 1... '
-h1 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.2 -p 5001 -t 20 > throughput_L1.txt
-h1 echo -e 'Done!\n'
+h1 echo -n 'Running Iperfer client on link 1'
+h1 /home/mininet/Private/CS640/P1/assign1/prog_dots.sh java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.2 -p 5001 -t 20 > throughput_L1.txt
+h1 echo -e ' Done!\n'
 
-h3 echo -n 'Running Iperfer client on link 2... '
-h3 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.2 -p 5001 -t 20 > throughput_L2.txt
-h3 echo -e 'Done!\n'
+h3 echo -n 'Running Iperfer client on link 2'
+h3 /home/mininet/Private/CS640/P1/assign1/prog_dots.sh java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.2 -p 5001 -t 20 > throughput_L2.txt
+h3 echo -e ' Done!\n'
 
-h4 echo -n 'Running Iperfer client on link 3... '
-h4 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.3 -p 5002 -t 20 > throughput_L3.txt
-h4 echo -e 'Done!\n'
+h4 echo -n 'Running Iperfer client on link 3'
+h4 /home/mininet/Private/CS640/P1/assign1/prog_dots.sh java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.3 -p 5002 -t 20 > throughput_L3.txt
+h4 echo -e ' Done!\n'
 
-h5 echo -n 'Running Iperfer client on link 4... '
-h5 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.3 -p 5002 -t 20 > throughput_L4.txt
-h5 echo -e 'Done!\n'
+h5 echo -n 'Running Iperfer client on link 4'
+h5 /home/mininet/Private/CS640/P1/assign1/prog_dots.sh java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.3 -p 5002 -t 20 > throughput_L4.txt
+h5 echo -e ' Done!\n'
 
-h6 echo -n 'Running Iperfer client on link 5... '
-h6 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.3 -p 5002 -t 20 > throughput_L5.txt
+h6 echo -n 'Running Iperfer client on link 5'
+h6 /home/mininet/Private/CS640/P1/assign1/prog_dots.sh java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.3 -p 5002 -t 20 > throughput_L5.txt
 h6 echo -e 'Done!\n'
 
 c0 echo -n 'Stopping servers... '
@@ -61,9 +61,9 @@ c0 echo -n 'Starting up Iperfer server on h1... '
 h1 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -s -p 5001 &
 c0 echo -e 'Done!\n'
 
-c0 echo -n 'Running Iperfer client on h4... '
-h4 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.1 -p 5001 -t 20 > throughput_Q2.txt
-c0 echo -e 'Done!\n'
+c0 echo -n 'Running Iperfer client on h4'
+h4 /home/mininet/Private/CS640/P1/assign1/prog_dots.sh java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.1 -p 5001 -t 20 > throughput_Q2.txt
+c0 echo -e ' Done!\n'
 
 c0 echo -n 'Stopping server on h1... '
 h1 pkill java
