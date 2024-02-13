4a4388eb95df956a1dddcce327bf47d92e063bba
M K <mkpro118@gmail.com>
2024-02-12 20:42:28 -0600

    Use MK's q3

--- P1/assign1/mn_q3_script.sh
+++ P1/assign1/mn_q3_script.sh
@@ -1,64 +1,67 @@
-c0 rm -fr answersL.txt
-c0 rm -fr answersT.txt
+c0 rm -fr MP*
+c0 echo -e '\nStarting Multiplexing Tests\n'
 
-c0 echo -e '\nmeasuring latency for simultaneous communication between hosts..\n'
+c0 echo 'Starting Latency tests'
 
-h8 ping 10.0.0.4 -c 20 -q > answersL.txt &
-h8 echo 'pinging h4 from h8'
-h7 ping 10.0.0.9 -c 20 -q >> answersL.txt &
-h7 echo 'pinging h9 from h7'
-h1 ping 10.0.0.10 -c 20 -q >> answersL.txt &
-h1 echo 'pinging h10 from h1'
+c0 echo -e 'Running Tests: h4 -> h1  +  h9 -> h7 \n'
+h4 ping 10.0.0.1 -c 20 -q > MP_h4_h1_1 &
+h4 echo 'Ping started on h4'
+h9 ping 10.0.0.7 -c 20 -q > MP_h9_h7_1 &
+h9 echo 'Ping started on h9'
 
-c0 echo -e '\nlatency measured for simultaneous communcation between hosts\n'
+c0 echo -e '\nWaiting for tests to complete'
+h4 wait; echo 'Ping on h4 terminated!'
+h9 wait; echo 'Ping on h9 terminated!'
 
-c0 echo -e '\nWaiting for hosts to finish\n'
-h8 wait $(jobs -p); echo 'h8 done!'
-h7 wait $(jobs -p); echo 'h7 done!'
-h1 wait $(jobs -p); echo 'h1 done!'
+c0 echo -e '\nRunning Tests: h4 -> h1  +  h9 -> h7  +  h10 -> h8 \n'
+h4 ping 10.0.0.1 -c 20 -q > MP_h4_h1_2 &
+h4 echo 'Ping started on h4'
+h9 ping 10.0.0.7 -c 20 -q > MP_h9_h7_2 &
+h9 echo 'Ping started on h9'
+h10 ping 10.0.0.8 -c 20 -q > MP_h10_h8_2 &
+h10 echo 'Ping started on h10'
 
-c0 echo -e '\nmeasuring throughput for simultaneous communication between hosts..n'
+c0 echo -e '\nWaiting for tests to complete'
+h4 wait; echo 'Ping on h4 terminated!'
+h9 wait; echo 'Ping on h9 terminated!'
+h10 wait; echo 'Ping on h10 terminated!'
 
-c0 echo -e 'Booting up Iperfer servers...\n'
-h8 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -s -p 5001 &
-h8 echo 'Server on h8 is up!'
-h7 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -s -p 5002 &
-h7 echo 'Server on h7 is up!'
-h1 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -s -p 5003 &
+c0 echo -e '\nLatency Tests Finished!'
+c0 echo -e '\nStarting Throughput tests\n'
+c0 echo -e 'Booting up servers\n'
+
+h1 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -s -p 5001 &
 h1 echo 'Server on h1 is up!'
-c0 echo -e '\nAll servers up and running\n'
+h7 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -s -p 5007 &
+h7 echo 'Server on h7 is up!'
+h8 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -s -p 5008 &
+h8 echo 'Server on h8 is up!'
 
+c0 echo -e '\nAll servers up and running'
 
-h4 echo -n 'Running Iperfer client between h8 and h4 ... '
-h4 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.8 -p 5001 -t 20 > answersT.txt &
+c0 echo -e '\nRunning Tests: h4 -> h1  +  h9 -> h7 \n'
+h4 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.1 -p 5001 -t 20 >> MP_h4_h1_1 &
+h4 echo 'Started client on h4'
+h9 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.7 -p 5007 -t 20 >> MP_h9_h7_1 &
+h9 echo 'Started client on h9'
 
+c0 echo -e '\nWaiting for tests to complete'
+h4 wait; echo 'Client on h4 terminated'
+h9 wait; echo 'Client on h9 terminated'
 
-h9 echo -n 'Running Iperfer client between h9 and h7 ... '
-h9 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.7 -p 5002 -t 20 >> answersT.txt &
+c0 echo -e '\nRunning Tests: h4 -> h1  +  h9 -> h7  +  h10 -> h8 \n'
+h4 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.1 -p 5001 -t 20 >> MP_h4_h1_2 &
+h4 echo 'Started client on h4'
+h9 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.7 -p 5007 -t 20 >> MP_h9_h7_2 &
+h9 echo 'Started client on h9'
+h10 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.8 -p 5008 -t 20 >> MP_h10_h8_2 &
+h10 echo 'Started client on h10'
 
+c0 echo -e '\nWaiting for tests to complete'
+h4 wait; echo 'Client on h4 terminated!'
+h9 wait; echo 'Client on h9 terminated!'
+h10 wait; echo 'Client on h10 terminated!'
 
-h10 echo -n 'Running Iperfer client between h10 and h1 ... '
-h10 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.1 -p 5003 -t 20 >> answersT.txt &
-
-
-
-c0 echo -e '\nWaiting for hosts to finish\n'
-h4 wait $(jobs -p); echo 'h4 done!'
-h9 wait $(jobs -p); echo 'h9 done!'
-h10 wait $(jobs -p); echo 'h10 done!'
-
-c0 echo -n 'Stopping server on h8... '
-h8 pkill java
-c0 echo -e 'Done\n'
-
-c0 echo -n 'Stopping server on h7... '
-h7 pkill java
-c0 echo -e 'Done\n'
-
-c0 echo -n 'Stopping server on h1... '
-h1 pkill java
-c0 echo -e 'Done\n'
-
-
-
+c0 echo -e '\nThroughput Tests Finished!'
+c0 echo -e '\nMultiplexing Tests Complete!'
 
