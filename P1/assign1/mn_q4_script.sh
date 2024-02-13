06560d81721c5340017cdc42590961631593dd04
M K <mkpro118@gmail.com>
2024-02-13 12:12:58 -0600

    dots for q4, fancy

--- P1/assign1/mn_q4_script.sh
+++ P1/assign1/mn_q4_script.sh
@@ -7,8 +7,8 @@ h5 ping 10.0.0.6 -c 20 -q > latency_h5-h6.txt &
 h5 echo 'pinging h6 from h5'
 
 c0 echo -e '\nWaiting for hosts to finish\n'
-h1 wait $(jobs -p); echo 'h1 done!'
-h5 wait $(jobs -p); echo 'h5 done!'
+h1 wait; echo 'h1 done!'
+h5 wait; echo 'h5 done!'
 
 c0 echo -e 'Booting up Iperfer servers...\n'
 h5 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -s -p 5001 &
@@ -23,14 +23,14 @@ h4 echo 'Running Iperfer client between h4 and h1 ... '
 h4 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.1 -p 5002 -t 20 > throughput_h1-h4.txt &
 
 c0 echo -e '\nWaiting for hosts to finish\n'
-h6 wait $(jobs -p); echo 'h6 done!'
-h4 wait $(jobs -p); echo 'h4 done!'
+h6 /home/mininet/Private/CS640/P1/assign1/prog_dots.sh wait; echo 'h6 done!'
+h4 wait; echo 'h4 done!'
 
 c0 echo 'Stopping server on h5... '
-h5 pkill java
+h5 pkill -INT java
 c0 echo -e 'Done\n'
 
 c0 echo -n 'Stopping server on h1... '
-h1 pkill java
+h1 pkill -INT java
 c0 echo -e 'Done\n'
 
