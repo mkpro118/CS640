5025dac6cfd3698c468cba2c85e6d98c22cd8d8e
M K <mkpro118@gmail.com>
2024-02-13 13:12:35 -0600

    maybe I'm not cut out for CS

--- P1/assign1/mn_q3_script.sh
+++ P1/assign1/mn_q3_script.sh
@@ -16,15 +16,13 @@ c0 echo -e '\nWaiting for tests to complete'
 h4 wait; echo 'Ping on h4 terminated!'
 h9 wait; echo 'Ping on h9 terminated!'
 
-
-
 c0 echo -e '\n3 Pair Ping\n' >> H4_LATENCY_LOGS
 c0 echo -e '\n3 Pair Ping\n' >> H9_LATENCY_LOGS
 
 c0 echo -e '\nRunning Tests: h4 -> h1  +  h9 -> h7  +  h10 -> h8 \n'
-h4 ping 10.0.0.1 -c 20 -q >> H4_LATENCY_LOGS &
-h9 ping 10.0.0.7 -c 20 -q >> H9_LATENCY_LOGS &
-h10 ping 10.0.0.8 -c 20 -q >> H10_LATENCY_LOGS &
+h4 /home/mininet/Private/CS640/P1/assign1/prog_dots.sh ping 10.0.0.1 -c 20 -q >> H4_LATENCY_LOGS &
+h9 /home/mininet/Private/CS640/P1/assign1/prog_dots.sh ping 10.0.0.7 -c 20 -q >> H9_LATENCY_LOGS &
+h10 /home/mininet/Private/CS640/P1/assign1/prog_dots.sh ping 10.0.0.8 -c 20 -q >> H10_LATENCY_LOGS &
 h4 echo 'Ping started on h4'
 h9 echo 'Ping started on h9'
 h10 echo 'Ping started on h10'
@@ -55,8 +53,8 @@ h9 echo 'Server on h9 is up!'
 c0 echo -e '\nAll servers up and running'
 
 c0 echo -e '\nRunning Tests: h1 -> h4  +  h7 -> h9 \n'
-h1 /home/mininet/Private/CS640/P1/prog_dots.sh java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.4 -p 5004 -t 20 &
-h7 /home/mininet/Private/CS640/P1/prog_dots.sh java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.9 -p 5009 -t 20 &
+h1 /home/mininet/Private/CS640/P1/assign1/prog_dots.sh java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.4 -p 5004 -t 20 &
+h7 /home/mininet/Private/CS640/P1/assign1/prog_dots.sh java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.9 -p 5009 -t 20 &
 h1 echo 'Started client on h4'
 h7 echo 'Started client on h9'
 
@@ -76,9 +74,9 @@ h10 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -s -p 5010 > H10_LOGS &
 c0 echo 'Done!'
 
 c0 echo -e '\nRunning Tests: h4 -> h1  +  h9 -> h7  +  h10 -> h8 \n'
-h1 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.4 -p 5004 -t 20 &
-h7 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.9 -p 5009 -t 20 &
-h8 java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.10 -p 5010 -t 20 &
+h1 /home/mininet/Private/CS640/P1/assign1/prog_dots.sh java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.4 -p 5004 -t 20 &
+h7 /home/mininet/Private/CS640/P1/assign1/prog_dots.sh java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.9 -p 5009 -t 20 &
+h8 /home/mininet/Private/CS640/P1/assign1/prog_dots.sh java -cp /home/mininet/Private/CS640/P1/bin/ Iperfer -c -h 10.0.0.10 -p 5010 -t 20 &
 h4 echo 'Started client on h4'
 h9 echo 'Started client on h9'
 h10 echo 'Started client on h10'
