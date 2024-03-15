7f4d912e8bfba9eeede2130170cbe7d337f272a3
M K <mkpro118@gmail.com>
2024-03-14 22:14:46 -0500

    Enable rip if static route table is not given

--- P3/src/edu/wisc/cs/sdn/vnet/Main.java
+++ P3/src/edu/wisc/cs/sdn/vnet/Main.java
@@ -88,6 +88,8 @@ public class Main
 			// Read static route table
 			if (routeTableFile != null)
 			{ ((Router)dev).loadRouteTable(routeTableFile); }
+			else
+			{ ((Router)dev).enableRIP(); }
 			
 			// Read static ACP cache
 			if (arpCacheFile != null)
