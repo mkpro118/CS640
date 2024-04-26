7acf61ea2c858c75509fffa4df1b9f57056b9dc5
M K <mkpro118@gmail.com>
2024-04-25 22:40:46 -0500

    Added more public methods

--- P4/ITCPPacket.java
+++ P4/ITCPPacket.java
@@ -1,5 +1,17 @@
 public interface ITCPPacket {
+    public short checksum();
+    public int getSequenceNumber();
+    public int getAcknowledgement();
+    public long getTimeStamp();
+
+    public boolean isSyn();
+    public boolean isAck();
+    public boolean isFin();
+    public void setFlag(TCPFlag flag, boolean on);
+
+    public byte[] getPayload();
+    public void setPayload(byte[] payload);
+
     public byte[] serialize();
     public ITCPPacket deserialize(byte[] packet);
-    public short checksum();
 }
